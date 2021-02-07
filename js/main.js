503f4ee4e78f1be3851f58503199e9a8ba3fe9f4
Андрей Кривонос <70647515+Krivonos-Andrew@users.noreply.github.com>
2021-02-07 19:53:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ function getRandomFloat(min, max) {
   const maxValue = Math.max(min, max);
   return Math.random() * (maxValue - minValue) + minValue;
 }
+// eslint-disable-next-line no-console
+console.log(getRandomFloat(1, 10));
+
+
 
 function getRandomInRange(min, max) {
   if (min === max) {
@@ -15,3 +19,5 @@ function getRandomInRange(min, max) {
   const maxValue = Math.max(min, max);
   return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 }
+// eslint-disable-next-line no-console
+console.log(getRandomInRange(1, 10));
