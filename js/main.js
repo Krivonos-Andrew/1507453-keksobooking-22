0151951ec938fcc02e90bf2ba29925e6dd1d33e2
Андрей Кривонос <70647515+Krivonos-Andrew@users.noreply.github.com>
2021-02-07 19:16:30 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+function getRandomFloat(min, max) {
+  if (min === max) {
+    return 0;
+  }
+  const minValue = Math.min(min, max);
+  const maxValue = Math.max(min, max);
+  return Math.random() * (max - min) + min;
+}
 
+function getRandomInRange(min, max) {
+  if (min === max) {
+    return 0;
+  }
+  const minValue = Math.min(min, max);
+  const maxValue = Math.max(min, max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
